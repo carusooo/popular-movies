70af8e8f80a7457027da6531806926b804e02f09
Andy Caruso <macaruso@gmail.com>
2018-05-15 20:58:20 -0700

    Convert JSON parsing to use Gson
    Use the Gson library and associated objects for JSON parsing.

--- app/src/main/java/com/example/macarus0/popularmovies/util/MovieJSONUtilities.java
+++ app/src/main/java/com/example/macarus0/popularmovies/util/MovieJSONUtilities.java
@@ -4,61 +4,72 @@ import android.content.ContentValues;
 import android.util.Log;
 
 import com.example.macarus0.popularmovies.data.MovieContract;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 public class MovieJSONUtilities {
-
-    private static final String TMDB_ID = "id";
-    private static final String TMDB_TITLE = "title";
-    private static final String TMDB_OVERVIEW = "overview";
-    private static final String TMDB_RELEASE_DATE = "release_date";
-    private static final String TMDB_USER_RATING = "vote_average";
-    private static final String TMDB_RUNTIME = "runtime";
-    private static final String TMDB_POSTER_PATH = "poster_path";
-    private static final String TMDB_POPULARITY = "popularity";
-
-    private static final String TMDB_RESULTS = "results";
 
     public MovieJSONUtilities() {}
 
     /*
      * This method parses the JSON returned from tMDB for the listing of popular movies
      */
-    public ContentValues[] parsePopularJSON(String jsonResponse) throws JSONException {
+    public ContentValues[] parsePopularJSON(String jsonResponse) {
+        Gson gson = new Gson();
+        PopularMoviePage popularMoviePage = gson.fromJson(jsonResponse, PopularMoviePage.class);
 
-        JSONObject popularJson = new JSONObject(jsonResponse);
-        JSONArray popularJsonArray = popularJson.getJSONArray(TMDB_RESULTS);
-
-        ContentValues[] popularContentValues = new ContentValues[popularJsonArray.length()];
-        Log.d("parsePopularJSON", String.format("Parsing %d popular movies", popularJsonArray.length()));
-        for (int i = 0; i < popularJsonArray.length(); i++) {
-            ContentValues movieContentValues = new ContentValues();
-            JSONObject movie = popularJsonArray.getJSONObject(i);
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_ID, movie.getInt(TMDB_ID));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_TITLE, movie.getString(TMDB_TITLE));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, movie.getString(TMDB_OVERVIEW));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, movie.getString(TMDB_RELEASE_DATE));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_USER_RATING, movie.getDouble(TMDB_USER_RATING));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_POSTER_PATH, movie.getString(TMDB_POSTER_PATH));
-            movieContentValues.put(MovieContract.MovieEntry.COLUMN_POPULARITY, movie.getString(TMDB_POPULARITY));
-            popularContentValues[i] = movieContentValues;
+        ContentValues[] popularContentValues = new ContentValues[popularMoviePage.results.length];
+        Log.d("parsePopularJSON", String.format("Parsing %d popular movies",popularMoviePage.results.length));
+        for (int i = 0; i < popularMoviePage.results.length; i++) {
+            popularContentValues[i] = popularMoviePage.results[i].toContentValues();
         }
-
         return popularContentValues;
     }
 
 
-    public ContentValues parseMovie(String movieJson) throws JSONException {
-        ContentValues movieContentValues = new ContentValues();
-
-        JSONObject movie = new JSONObject(movieJson);
-        movieContentValues.put(MovieContract.MovieEntry.COLUMN_ID, movie.getInt(TMDB_ID));
-        movieContentValues.put(MovieContract.MovieEntry.COLUMN_RUNTIME, movie.getString(TMDB_RUNTIME));
-
+    public ContentValues parseMovie(String movieJson) {
+        Gson gson = new Gson();
+        MovieDetails movieDetails = gson.fromJson(movieJson, MovieDetails.class);
+        ContentValues movieContentValues = movieDetails.toContentValues();
         return movieContentValues;
+    }
+
+    class PopularMoviePage {
+        PopularMovie[] results;
+    }
+
+    class PopularMovie {
+        int id;
+        String title;
+        String overview;
+        String release_date;
+        float vote_average;
+        String poster_path;
+        String popularity;
+
+        public ContentValues toContentValues() {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MovieContract.MovieEntry.COLUMN_ID, this.id);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_TITLE, this.title);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_OVERVIEW, this.overview);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_RELEASE_DATE, this.release_date);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_USER_RATING, this.vote_average);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_POSTER_PATH, this.poster_path);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_POPULARITY, this.popularity);
+            return contentValues;
+
+        }
+    }
+
+    class MovieDetails {
+        int id;
+        String runtime;
+
+        public ContentValues toContentValues() {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MovieContract.MovieEntry.COLUMN_ID, this.id);
+            contentValues.put(MovieContract.MovieEntry.COLUMN_RUNTIME, this.runtime);
+            return contentValues;
+        }
     }
 
 }
